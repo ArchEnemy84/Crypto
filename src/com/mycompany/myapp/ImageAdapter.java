a3999034a9963d9751ddf4fd95538eb730a61f9c
ArchEnemy84 <patrick.stuckenberg@gmx.net>
2014-07-07 16:38:19 +0200

    changed ArrayList to generic ArrayList<T>

--- src/com/mycompany/myapp/ImageAdapter.java
+++ src/com/mycompany/myapp/ImageAdapter.java
@@ -1,7 +1,7 @@
 package com.mycompany.myapp;
 
 import android.view.*;
-import android.util.ArrayList;
+import android.util.ArrayList<T>;
 import android.net.Uri;
 import android.widget.BaseAdapter;
 import android.content.Context;
