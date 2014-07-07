4808a5acd721b33fc21e410d63823721a3f63707
ArchEnemy84 <patrick.stuckenberg@gmx.net>
2014-07-07 15:36:04 +0200

    added src for ProcessSingle.java

--- src/com/mycompany/myapp/ProcessSingle.java
+++ src/com/mycompany/myapp/ProcessSingle.java
@@ -1,7 +1,17 @@
 package com.mycompany.myapp;
 
 import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
 public class ProcessSingle extends Activity
 {
-	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 }
