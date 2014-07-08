ae772ac2a266dc2f8b6826aa5fcfb0999054f41e
Patrick Stuckenberg <patrick.stuckenberg@gmx.net>
2014-07-08 09:30:02 +0200

    changed imageadapter to thumbnail

--- src/com/mycompany/myapp/ImageAdapter.java
+++ src/com/mycompany/myapp/ImageAdapter.java
@@ -34,7 +34,7 @@ public class ImageAdapter extends BaseAdapter {
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
-        boolean thumb = false;
+        boolean thumb = true;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, 100));
